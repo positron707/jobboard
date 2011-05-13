ea81180d8d18629bedda7873217166e4d251f262
Parag Dave <positron707@gmail.com>
2011-05-12 22:45:12 -0400

    commit

---
+++ db/migrate/003_flag_and_last_login_to_user.rb
@@ -0,0 +1,11 @@
+class FlagAndLastLoginToUser < ActiveRecord::Migration
+  def self.up
+	add_column :users, :admin, :boolean :default => false
+    add_column :users, :last_login, :timestamp
+  end
+
+  def self.down
+	remove_column :users, :last_login
+    remove_column :users, :admin
+  end
+end
